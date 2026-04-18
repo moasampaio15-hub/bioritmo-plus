42ee025a4d6fbcb24dfccb070806b1e05ac4d00c
Dr. Moacir Sampaio <moasampaio15@gmail.com>
2026-04-18 07:35:11 -0300

    Fix: Atualiza better-sqlite3 para v11 com binarios pre-compilados

--- core/database/connection.js
+++ core/database/connection.js
@@ -1,12 +1,4 @@
-// Usar bun:sqlite se disponível (Render com Bun), senão better-sqlite3
-let Database;
-try {
-    Database = require('bun:sqlite').Database;
-    console.log('[DB] Usando bun:sqlite');
-} catch {
-    Database = require('better-sqlite3');
-    console.log('[DB] Usando better-sqlite3');
-}
+const Database = require('better-sqlite3');
 const path = require('path');
 const fs = require('fs');
 
